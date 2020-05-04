6dafe544f5bc081098989a959c63884863d4d544
laurent r berge <laurent.berge@hotmail.fr>
2020-05-04 09:00:10 +0200

    Add onLoad for setFplot_page options

---
+++ R/onLoad.R
@@ -0,0 +1,11 @@
+
+
+
+.onLoad <- function(libname, pkgname){
+	# setting some options
+
+	options("fplot_img_opts" = list(page_dim = c(8.5, 11), page_dim_net = c(6.531496, 9.425197), units = "tw"))
+
+	invisible()
+}
+
